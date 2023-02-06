54d2e48b23049f181a57859fb9690b3474f69949
dejanwebtech <dejanmatic.dev@gmail.com>
2023-02-06 22:39:27 +0100

    Added higher container

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,6 @@
 function App() {
     return (
-        <div className="grid-container test">
+        <div className="grid-container higher-container">
             <header>
                 <a href="/"> React Shoping cart</a>
             </header>
